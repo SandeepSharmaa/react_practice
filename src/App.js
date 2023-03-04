f0a1d1cc95821a7c6162ff3362efc9f812ab8c0f
SandeepSharmaa <app@aham.health>
2023-03-04 18:17:50 +0530

    sign up form ui

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
-import { Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Registration from './ui/login/login';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload .
-        </p>
-        <Button>Submit</Button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+     <Registration/>
+
     </div>
   );
 }
